c01f0adff06e1ba51f6d58ec1b345cb884e1d51e
Himanshu bisht <Himanshubxt02@gmail.com>
2022-11-17 19:48:06 +0530

    Add files via upload

---
+++ c++(DSA)/Bitwise_operator/5-questions/Qus1.cpp
@@ -0,0 +1,18 @@
+//output?
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    for(int i = 0; i<=5; i++){
+        cout<<i<<""<<endl;
+        i++;
+    }
+
+}
+
+
+/*output will be 
+0
+2
+4*/
---
+++ c++(DSA)/Bitwise_operator/scope.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int a = 3;
+int main(){
+     cout<< a <<endl;
+
+    if(true){
+        int a = 5;
+        cout<<a<<endl;
+    }
+    cout<<a<<endl;
+}
+
+/*output will
+3
+5
+3
+*/
